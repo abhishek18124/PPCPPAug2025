e2d756abca51b1a802a6ab722c685e7f35256bb3
Abhishek Srivastava <abhishek18124@iiitd.ac.in>
2025-10-04 20:12:19 +0530

    Lecture 12

---
+++ Lecture 12/001Arrays_BubbleSort2.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+
+	for (int i = 1; i <= n - 1; i++) {
+
+		// for the ith pass, place the largest
+		// element in the unsorted part of the
+		// arr[] to its correct position
+
+		bool flag = false; // assume no swaps will be done in the ith pass
+
+		for (int j = 0; j < n - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+				flag = true;
+			}
+		}
+
+		if (flag == false) {
+			break; // since no swaps were done in the current pass, it means array is already sorted, no need to run bubble sort for more passes
+		}
+
+	}
+
+}
+
+int main() {
+
+	int arr[] = {50, 40, 30, 20, 10};
+	int n = sizeof(arr) / sizeof(int);
+
+	bubbleSort(arr, n);
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+
+	return 0;
+}
